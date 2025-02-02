b0a94cb8e7bb149c8264f2331e0dea160ec2b13b
victorpantarotti <victorpantarotti@gmail.com>
2025-02-02 12:13:04 -0300

    Update Who am I Text and Photo

--- src/locales/en-US/HomePage.ts
+++ src/locales/en-US/HomePage.ts
@@ -2,5 +2,5 @@ export default {
     greeting: "howdy, i'm",
     greetingCompliment: "you good? :)",
     whoAmITitle: "Who am I?",
-    whoAmIText: `Hello! I'm Victor Hugo, I'm 16 years old and a Junior Front-End Developer, based in São Paulo, Brazil.\nSince a young age, my admiration for technology has been clear. In 2020, I entered the world of programming and, using Java Bukkit, I created plugins for Minecraft game servers. In the same year, I started building bots for the Discord platform, using Javascript. However, in 2021, due to unexpected issues with bot development, I decided to focus on Front-End. Since then, I have been dedicating myself to learning and improving my skills in building dynamic and functional interfaces.`
+    whoAmIText: `Hello! I'm Victor Hugo, I'm 16 years old and a Front-End Developer, based in São Paulo, Brazil.\nSince a young age, my admiration for technology has been clear. In 2020, I entered the world of programming and, using Java Bukkit, I created plugins for Minecraft game servers. In the same year, I started building bots for the Discord platform, using Javascript. However, in 2021, due to unexpected issues with bot development, I decided to focus on Front-End. Since then, I have been dedicating myself to learning and improving my skills in building dynamic and functional interfaces.`
 }
